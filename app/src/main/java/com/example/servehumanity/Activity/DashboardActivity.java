202b10e2d20824207b61741b89965fb80754af6b
Bikash Dhakal <dhakalbikash0@gmail.com>
2020-08-17 15:00:21 +0545

    'fragment adapter created'

--- app/src/main/java/com/example/servehumanity/Activity/DashboardActivity.java
+++ app/src/main/java/com/example/servehumanity/Activity/DashboardActivity.java
@@ -1,16 +1,33 @@
 package com.example.servehumanity.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import com.example.servehumanity.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#ffffff"));
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), FragmentAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragmentAdapter.addFragment(new HomeFragment(), "Home");
+        fragmentAdapter.addFragment(new AddStudentFragment(), "Add Student");
+        fragmentAdapter.addFragment(new AboutUsFragment(), "About Us");
+
+        viewPager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        setUpIcons();
     }
 }
---
+++ app/src/main/java/com/example/servehumanity/adapter/FragmentAdapter.java
@@ -0,0 +1,40 @@
+package com.example.servehumanity.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fr = new ArrayList<>();
+    private  List<String> fragTitle = new ArrayList<>();
+    public FragmentAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fr.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fr.size();
+    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fr.add(fragment);
+        fragTitle.add(title);
+    }
+}
