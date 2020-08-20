9eb81a216fcf35e73943b12de6160f4e63e937b3
Bikash Dhakal <dhakalbikash0@gmail.com>
2020-08-20 23:26:25 +0545

    'Emergency activity created'

--- app/src/main/java/com/example/servehumanity/Activity/EmergencyActivity.java
+++ app/src/main/java/com/example/servehumanity/Activity/EmergencyActivity.java
@@ -3,14 +3,19 @@ package com.example.servehumanity.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.example.servehumanity.R;
 
 public class EmergencyActivity extends AppCompatActivity {
-
+WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+
+        WebView mywebview = (WebView) findViewById(R.id.webView);
+
+        mywebview.loadUrl("https://www.nepalpolice.gov.np/index.php/safety-security-tips/emergency-numbers");
     }
 }
