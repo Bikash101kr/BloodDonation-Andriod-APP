3ef51a7e1134ad8892c37a8c6a8bb640f58bd846
Bikash Dhakal <dhakalbikash0@gmail.com>
2020-08-20 23:23:11 +0545

    'Donation camps activity created'

--- app/src/main/java/com/example/servehumanity/Activity/DonationCampsActivity.java
+++ app/src/main/java/com/example/servehumanity/Activity/DonationCampsActivity.java
@@ -3,14 +3,23 @@ package com.example.servehumanity.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.example.servehumanity.R;
 
 public class DonationCampsActivity extends AppCompatActivity {
-
+    WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_camps);
+        WebView mywebview = (WebView) findViewById(R.id.webView);
+
+        mywebview.loadUrl("https://nrcs.org/donate-blood/");
+
     }
+
 }
