b83c6794014fa862db53a0c83204f48ac22a8af6
Bikash Dhakal <dhakalbikash0@gmail.com>
2020-09-25 12:06:18 +0545

    'all completed'

--- app/src/main/java/com/example/servehumanity/Activity/EmergencyActivity.java
+++ app/src/main/java/com/example/servehumanity/Activity/EmergencyActivity.java
@@ -1,21 +1,21 @@
 package com.example.servehumanity.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.servehumanity.R;
 
 public class EmergencyActivity extends AppCompatActivity {
-WebView webView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_emergency);
+  WebView webView;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_emergency);
 
-        WebView mywebview = (WebView) findViewById(R.id.webView);
+    WebView mywebview = (WebView) findViewById(R.id.webView);
 
-        mywebview.loadUrl("https://www.nepalpolice.gov.np/index.php/safety-security-tips/emergency-numbers");
-    }
+    mywebview.loadUrl("https://www.nepalpolice.gov.np/index.php/safety-security-tips/emergency-numbers");
+  }
 }
